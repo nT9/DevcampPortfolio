9f900fc2a59f6002696b248bb6ab46876f2b6eec
slcwestdevcamp <slcwest@devcamp.com>
2017-06-22 16:12:49 +0000

    FIXED

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,29 @@
 10.times do |blog|
-    Blog.create!(
+  Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "This is the body."
-    )
+    body: "gibberish"
+  )
 end
 
 puts "10 blog posts created"
 
 5.times do |skill|
-    Skill.create!(
+  Skill.create!(
     title: "Rails #{skill}",
     percent_utilized: 15
-    )
+  )
 end
 
 puts "5 skills created"
 
 9.times do |portfolio_item|
-    Portfolio.create!(
-        title: "Portfolio title #{portfolio_item}",
-        subtitle: "My service",
-        body: "blah blah blah blah",
-        main_image: "http://placehold.it/600x400",
-        thumb_image: "http://placehold.it/350x200",
-    )
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Smol title",
+    body: "moar gibberish",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
 end
 
-puts "9 skills created"
+puts "9 portfolio items created"
