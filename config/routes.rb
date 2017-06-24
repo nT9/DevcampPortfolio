dd7773f882fbd8d7a9cdee5f5ff750a059a58949
slcwestdevcamp <slcwest@devcamp.com>
2017-06-24 21:25:48 +0000

    Custom scopes portfolios

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
+  
+  get "angular-items", to: "portfolio#angular"
+  
   get "portfolio/:id", to: "portfolios#show", as: "portfolio_show"
   
   get 'about-me', to: "pages#about"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"  
+  )
+end
+
+puts "3 topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "gibberish"
+    body: "gibberish",
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,14 +25,25 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "Smol title",
+    subtitle: "Ruby Rails",
     body: "moar gibberish",
     main_image: "http://placehold.it/600x400",
     thumb_image: "http://placehold.it/350x200"
   )
 end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
+    body: "moar gibberish",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
+
 
 puts "9 portfolio items created"
