b8ab60fdf490d073fb51b676ec3af7de77e87a7a
slcwestdevcamp <slcwest@devcamp.com>
2017-06-27 16:15:46 +0000

    Added session tracker for dynamic source links

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   get "portfolio/:id", to: "portfolios#show", as: "portfolio_show"
   
   get 'about-me', to: "pages#about"
-  
   get 'contact', to: "pages#contact"
 
   resources :blogs do
