e5cef36e058fe95241accab6d7beeecd09c3311d
slcwestdevcamp <slcwest@devcamp.com>
2017-08-06 00:38:42 +0000

    Implemented partial for admin actions

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -22,4 +22,8 @@ module BlogsHelper
     markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
     markdown_to_html.render(text).html_safe
   end
+  
+  def blog_status_color(blog)
+      "color: #851545;" if blog.draft?
+  end
 end
